36dba0d63bee568331f9d23ec65188a4dd3e7131
Solomon Sunday <ssdahymond@gmail.com>
2020-10-30 19:26:05 +0100

    completed the admin apis

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -4,16 +4,16 @@ from django.urls import reverse
 
 
 class AdminSiteTests(TestCase):
-    def SetUp(self):
+
+    def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@dahymond.com',
+            email='admin@londonappdev.com',
             password='password123'
         )
-
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='test@dahymond.com',
+            email='test@londonappdev.com',
             password='password123',
             name='Test user full name'
         )
@@ -22,12 +22,11 @@ class AdminSiteTests(TestCase):
         """Test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
-
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-    def test_user_change_page(self):
-        """Test that the user edit page works"""
+    def test_user_Change_page(self):
+        """TEst that the user edit page works"""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
